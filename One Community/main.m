1fd658d0b82955b9d78b1deefe1332ab292cd585
Gustavo Nogueira <gustavo.nogueira@mowa.com.br>
2015-06-01 09:41:21 -0700

    First Commit

---
+++ One Community/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  One Community
+//
+//  Created by Gustavo Romito on 6/1/15.
+//  Copyright (c) 2015 Gustavo Romito. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+   @autoreleasepool {
+       return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+   }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+# Uncomment this line to define a global platform for your project
+# platform :ios, '6.0'
+
+target 'One Community' do
+	pod 'ZBarSDK'
+end
+
+target 'One CommunityTests' do
+
+end
+
